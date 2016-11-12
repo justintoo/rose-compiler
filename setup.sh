9ee89c116b695fb8ca48fbac457f79b4df1f1282
Justin Too <doubleotoo@gmail.com>
2016-11-11 18:01:14 -0800

    Use built-in cd function and not a user alias

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-export ROSE_SH_HOME="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+# Escape \cd to use the native cd and not a user's possible alias of cd
+export ROSE_SH_HOME="$(\cd "$(dirname "${BASH_SOURCE}")" && pwd)"
 export PATH="${ROSE_SH_HOME}/bin:${PATH}"
